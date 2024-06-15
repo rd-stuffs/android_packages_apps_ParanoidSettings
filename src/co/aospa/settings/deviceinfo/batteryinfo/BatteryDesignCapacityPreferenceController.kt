6bafaf689aa1925f7f2eb157702c770a3ef8045f
Richard Raya <rdxzv.dev@gmail.com>
2024-06-15 16:23:36 -0300

    ParanoidSettings: Make battery design capacity configurable
    Change-Id: I2a2c4f43a9cd8a4972b2b1c239350d05889f3679
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- src/co/aospa/settings/deviceinfo/batteryinfo/BatteryDesignCapacityPreferenceController.kt
+++ src/co/aospa/settings/deviceinfo/batteryinfo/BatteryDesignCapacityPreferenceController.kt
@@ -19,7 +19,11 @@ class BatteryDesignCapacityPreferenceController(
     preferenceKey: String
 ) : BasePreferenceController(context, preferenceKey) {
 
-    override fun getAvailabilityStatus(): Int = AVAILABLE
+    override fun getAvailabilityStatus(): Int =
+        if (mContext.resources.getBoolean(R.bool.config_showBatteryDesignCapacity))
+            AVAILABLE
+        else
+            UNSUPPORTED_ON_DEVICE
 
     override fun getSummary(): CharSequence {
         val batteryIntent = BatteryUtils.getBatteryIntent(mContext)
